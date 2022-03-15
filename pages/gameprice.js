e900fcce7fca710beb2f5981c1b9d5ac6298afd5
tammawatRa <tammawat.ra@gmail.com>
2022-03-15 16:58:24 +0700

    Update gameprice.js

--- pages/gameprice.js
+++ pages/gameprice.js
@@ -64,60 +64,44 @@ function favorite() {
               </h2> */}
               <div class="flex mb-4">
                 <span class="flex items-center">
-                  <span class="text-gray-600 ml-3">Developer: Santa Monica Studio</span>
+                  <span class="text-gray-600 ml-3">
+                    ผู้พัฒนา: Santa Monica Studio
+                  </span>
                 </span>
                 <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                  <span class="text-gray-600 ml-3">Publisher: PlayStation PC LLC</span>
+                  <span class="text-gray-600 ml-3">
+                    ผู้จัดจำหน่าย: PlayStation PC LLC
+                  </span>
                 </span>
                 <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                  <span class="text-gray-600 ml-3">Release Date: 14 ม.ค. 2022</span>
+                  <span class="text-gray-600 ml-3">
+                    วันวางจำหน่าย: 14 ม.ค. 2022
+                  </span>
                 </span>
                 <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                  <span class="text-gray-600 ml-3">Required Age: 18</span>
+                  <span class="text-gray-600 ml-3">
+                    อายุที่ต้องการขั้นต่ำ: 18
+                  </span>
                 </span>
               </div>
               <p class="leading-relaxed">
-              การชำระแค้นกับเหล่าทวยเทพแห่งโอลิมปัสผ่านพ้นมาหลายปีแล้ว บัดนี้เครโทสใช้ชีวิตเยี่ยงชายคนหนึ่งในดินแดนของเหล่าเทพเจ้าแห่งนอร์สและหมู่มวลสัตว์ประหลาด ในโลกอันโหดร้ายและไม่ปรานีปราศรัยแห่งนี้ เขาต้องต่อสู้เพื่อเอาชีวิตรอด… และต้องสอนลูกชายให้ทำแบบเดียวกันด้วย
+                การชำระแค้นกับเหล่าทวยเทพแห่งโอลิมปัสผ่านพ้นมาหลายปีแล้ว
+                บัดนี้เครโทสใช้ชีวิตเยี่ยงชายคนหนึ่งในดินแดนของเหล่าเทพเจ้าแห่งนอร์สและหมู่มวลสัตว์ประหลาด
+                ในโลกอันโหดร้ายและไม่ปรานีปราศรัยแห่งนี้
+                เขาต้องต่อสู้เพื่อเอาชีวิตรอด…
+                และต้องสอนลูกชายให้ทำแบบเดียวกันด้วย
               </p>
-              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                <div class="flex">
-                  <span class="mr-3">Color</span>
-                  <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                </div>
-                <div class="flex ml-6 items-center">
-                  <span class="mr-3">Size</span>
-                  <div class="relative">
-                    <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-4 h-4"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </div>
+
+              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
+
               <div class="flex">
                 <span class="title-font font-medium text-2xl text-gray-900">
-                  $58.00
+                  ราคาถูกสุด: <span className="text-blue-500">1232.13</span> บาท
                 </span>
                 <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                  Button
+                  ติดตามราคาเกม
                 </button>
+
                 <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
@@ -131,6 +115,150 @@ function favorite() {
                   </svg>
                 </button>
               </div>
+
+              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
+              {/* -----ส่วนจัดเรียงราคา----- */}
+              <div class="lg:w-full w-full mx-auto overflow-auto">
+                <table class="table-auto w-full text-left whitespace-no-wrap">
+                  <thead>
+                    <tr>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                        ร้านค้า
+                      </th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                        {/* Speed */}
+                      </th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                        {/* Storage */}
+                      </th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                        ราคา
+                      </th>
+                      {/* <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th> */}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="px-4 py-3">GameSrig</td>
+                      <td class="px-4 py-3">{/* 5 Mb/s */}</td>
+                      <td class="px-4 py-3">{/* 15 GB */}</td>
+                      <td class="px-4 py-3 text-lg text-gray-900">$12</td>
+                      {/* <td class="w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                    <tr>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">Hrk</td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 25 Mb/s */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 25 GB */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                        $24
+                      </td>
+                      {/* <td class="border-t-2 border-gray-200 w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                    <tr>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        Kinguin
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 36 Mb/s */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 40 GB */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                        $50
+                      </td>
+                      {/* <td class="border-t-2 border-gray-200 w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                    <tr>
+                      <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                        Steam
+                      </td>
+                      <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                        {/* 48 Mb/s */}
+                      </td>
+                      <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                        {/* 120 GB */}
+                      </td>
+                      <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                        $72
+                      </td>
+                      {/* <td class="border-t-2 border-b-2 border-gray-200 w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                    <tr>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        Epicgames
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 36 Mb/s */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 40 GB */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                        $50
+                      </td>
+                      {/* <td class="border-t-2 border-gray-200 w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                    <tr>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        Gamivo
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 36 Mb/s */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3">
+                        {/* 40 GB */}
+                      </td>
+                      <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                        $50
+                      </td>
+                      {/* <td class="border-t-2 border-gray-200 w-10 text-center">
+                        <input name="plan" type="radio" />
+                      </td> */}
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              {/* จบส่วนจัดเรียงราคา */}
+              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
+            </div>
+            {/* ส่วนสเปค */}
+            <div class="md:w-1/2 md:pr-17 md:pl-8 md:py-1 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                สเปคขั้นต่ำ
+              </h1>
+              <p class="leading-relaxed text-base">
+                Locavore cardigan small batch roof party blue bottle blog
+                meggings sartorial jean shorts kickstarter migas sriracha
+                church-key synth succulents. Actually taiyaki neutra, distillery
+                gastropub pok pok ugh.
+              </p>
+            </div>
+
+            <div class="flex flex-col md:w-1/2 md:pl-12 md:pl-8 md:pr-17">
+              <h class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                สเปคพื้นฐาน
+              </h>
+              <p class="leading-relaxed text-base">
+                Locavore cardigan small batch roof party blue bottle blog
+                meggings sartorial jean shorts kickstarter migas sriracha
+                church-key synth succulents. Actually taiyaki neutra, distillery
+                gastropub pok pok ugh.
+              </p>
             </div>
           </div>
         </div>
