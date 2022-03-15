e2b2675861df93fb105976528d0554f500e61ac5
tammawatRa <tammawat.ra@gmail.com>
2022-03-15 15:22:50 +0700

    back-up Game-price

--- pages/gameprice.js
+++ pages/gameprice.js
@@ -1,26 +1,142 @@
 import Navbar from "../components/navbar";
-import  Link  from "next/link";
-import React, { useState } from "react";
+import Link from "next/link";
 import Axios from "axios";
+import { useState, useRef, Component } from "react";
+import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function favorite() {
-    const [gamesdataList, setGamesdataList] = useState([]);
+  const [gamesdataList, setGamesdataList] = useState([]);
   const getGamesdata = () => {
     Axios.get("http://localhost:3001/games_data").then((response) => {
       setGamesdataList(response.data);
     });
   };
-    
-    return (
-        
-      <div className="">
-        <Navbar />
 
-        <div className=" flex-col pt-40 text-left lg:text-4xl text-5xl space-y-2">
-          <h1 className="ml-10">ชื่อเกม  </h1>
+  return (
+    <div className="">
+      <Navbar />
+
+      <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
+              <Carousel>
+                <div>
+                  <img
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/ss_6eccc970b5de2943546d93d319be1b5c0618f21b.1920x1080.jpg?t=1642526157"
+                    alt="image1"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/ss_f1bff24d3967a21d303d95e11ed892e3d9113057.1920x1080.jpg?t=1642526157"
+                    alt="image2"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/ss_3670ba72c7e3e9c3c3225547ef2c1053504e62b8.1920x1080.jpg?t=1642526157"
+                    alt="image3"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/ss_93a3ca63aa2cd8c675bbb6430324ee3f2d44b845.1920x1080.jpg?t=1642526157"
+                    alt="image4"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/ss_1bd99270dcbd4ff9fe9c94b0d9c8ffc50ebb42c7.1920x1080.jpg?t=1642526157"
+                    alt="image5"
+                  />
+                </div>
+              </Carousel>
+            </div>
+            <div class="lg:w-full w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
+                God of War
+              </h1>
+              {/* <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                Release Date
+              </h2> */}
+              <div class="flex mb-4">
+                <span class="flex items-center">
+                  <span class="text-gray-600 ml-3">Developer: Santa Monica Studio</span>
+                </span>
+                <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                  <span class="text-gray-600 ml-3">Publisher: PlayStation PC LLC</span>
+                </span>
+                <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                  <span class="text-gray-600 ml-3">Release Date: 14 ม.ค. 2022</span>
+                </span>
+                <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                  <span class="text-gray-600 ml-3">Required Age: 18</span>
+                </span>
+              </div>
+              <p class="leading-relaxed">
+              การชำระแค้นกับเหล่าทวยเทพแห่งโอลิมปัสผ่านพ้นมาหลายปีแล้ว บัดนี้เครโทสใช้ชีวิตเยี่ยงชายคนหนึ่งในดินแดนของเหล่าเทพเจ้าแห่งนอร์สและหมู่มวลสัตว์ประหลาด ในโลกอันโหดร้ายและไม่ปรานีปราศรัยแห่งนี้ เขาต้องต่อสู้เพื่อเอาชีวิตรอด… และต้องสอนลูกชายให้ทำแบบเดียวกันด้วย
+              </p>
+              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                <div class="flex">
+                  <span class="mr-3">Color</span>
+                  <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                </div>
+                <div class="flex ml-6 items-center">
+                  <span class="mr-3">Size</span>
+                  <div class="relative">
+                    <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                      <option>SM</option>
+                      <option>M</option>
+                      <option>L</option>
+                      <option>XL</option>
+                    </select>
+                    <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M6 9l6 6 6-6"></path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="flex">
+                <span class="title-font font-medium text-2xl text-gray-900">
+                  $58.00
+                </span>
+                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  Button
+                </button>
+                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    );
-  }
-  
-  export default favorite;
+      </section>
+    </div>
+  );
+}
+
+export default favorite;
